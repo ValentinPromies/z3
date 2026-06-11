e529141e6ee5b08671a1716a1427c5d326899514
Valentin Promies <valentin.promies@rwth-aachen.de>
2026-06-11 13:26:23 +0200

    add option to compute linear cells later

--- src/math/lp/nla_core.cpp
+++ src/math/lp/nla_core.cpp
@@ -1342,7 +1342,23 @@ lbool core::check(unsigned level) {
 
     if (no_effect()) 
         m_divisions.check();
-
+    
+    if (no_effect() && params().arith_nl_nra_check_assignment_first()) {
+        if (m_check_assignment_fail_cnt >= params().arith_nl_nra_check_assignment_max_fail()) {
+            ++m_check_assignment_fail_cnt;
+            if (m_check_assignment_fail_cnt >= params().arith_nl_nra_check_assignment_max_fail() + m_block_check_assignment) {
+                m_check_assignment_fail_cnt = 0;
+                m_block_check_assignment *= 2;
+            }
+        } else {
+            ++lp_settings().stats().m_check_assignment_calls;
+            scoped_limits sl(m_reslim);
+            sl.push_child(&m_nra_lim);
+            ret = m_nra.check_assignment();
+            if (ret != l_true)
+                ++m_check_assignment_fail_cnt;
+        }
+    }
 
     if (no_effect()) {
         std::function<void(void)> check1 = [&]() { m_order.order_lemma();
@@ -1363,7 +1379,7 @@ lbool core::check(unsigned level) {
             ret = bounded_nlsat();
     }
 
-    if (no_effect() && params().arith_nl_nra_check_assignment()) {
+    if (no_effect() && params().arith_nl_nra_check_assignment_later()) {
         if (m_check_assignment_fail_cnt >= params().arith_nl_nra_check_assignment_max_fail()) {
             ++m_check_assignment_fail_cnt;
             if (m_check_assignment_fail_cnt >= params().arith_nl_nra_check_assignment_max_fail() + m_block_check_assignment) {
